12c733761b6d8cf4445030f28dffb492bf569d1e
riyakush0611 <98538208+riyakush0611@users.noreply.github.com>
2024-09-24 23:09:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ let pathControl;
 let pathPoints = [];
 
 // Replace 'YOUR_MAPBOX_TOKEN' with an actual token
-const accessKey = 'pk.eyJ1IjoibGlsZXNoIiwiYSI6ImNsemp4ZTc0MzB0aDIya3IxMXF1NWJvbzgifQ.E4mLxZLZCph5ohJB6rtW9w';
+const accessKey = 'sk.gsdvRhgngnhmhmmjiwiYSI6ImNsemp4ZTc0MzB0aDIya3IxMXF1NWJvbzgifQ.E4mLxZLZCph58edgsbgdn8ut';
 
 // Add event listener to display route based on the selected trip
 document.getElementById('draw-route').addEventListener('click', () => {
